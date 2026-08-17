b890f94b3455db1f3be28d7f1e7d9c9bd25dd5db
ShivanandMegur18 <shivanandmeguer@gmail.com>
2026-08-17 18:45:24 +0530

    prime number program

--- prime.c
+++ prime.c
@@ -1,23 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n,i,count=1;
-    printf("enter the n value : \n");
-    scanf("%d",&n);
-    // if(n<=1)
-    //     printf("it not prime");
+int main() {
+    int n, i, count = 0;
 
-    for(i=1; i<=n%2; i++){
-        if(n%i == 0){
+    printf("Enter the n value: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("Not prime\n");
+        return 0;
+    }
+
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {
             count++;
-            printf("%d\n",count);
         }
     }
-    if(count == 2){
-        printf("its primr\n");
+
+    if (count == 2) {
+        printf("It is prime\n");
+    } else {
+        printf("Not prime\n");
     }
-    else{
-        printf("not primt\n");
-    }
+
     return 0;
 }
